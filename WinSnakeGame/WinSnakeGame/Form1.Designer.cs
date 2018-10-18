a11dd2b086d3a7a8c70bb2a81aefc4cd354d6b15
BuildTools <unconfigured@null.spigotmc.org>
2018-10-18 19:33:20 +1300

    update speed of snake and time intervals to 20ms from 100ms.

--- WinSnakeGame/WinSnakeGame/Form1.Designer.cs
+++ WinSnakeGame/WinSnakeGame/Form1.Designer.cs
@@ -40,42 +40,47 @@
             // Update
             // 
             this.Update.Enabled = true;
+            this.Update.Interval = 20;
             this.Update.Tick += new System.EventHandler(this.Update_Tick);
             // 
             // pb_playerHead
             // 
             this.pb_playerHead.BackColor = System.Drawing.Color.Green;
-            this.pb_playerHead.Location = new System.Drawing.Point(351, 260);
+            this.pb_playerHead.Location = new System.Drawing.Point(702, 500);
+            this.pb_playerHead.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pb_playerHead.Name = "pb_playerHead";
-            this.pb_playerHead.Size = new System.Drawing.Size(25, 25);
+            this.pb_playerHead.Size = new System.Drawing.Size(50, 48);
             this.pb_playerHead.TabIndex = 1;
             this.pb_playerHead.TabStop = false;
             // 
             // lbl_currentKeyPressCheck
             // 
             this.lbl_currentKeyPressCheck.AutoSize = true;
-            this.lbl_currentKeyPressCheck.Location = new System.Drawing.Point(348, 9);
+            this.lbl_currentKeyPressCheck.Location = new System.Drawing.Point(696, 17);
+            this.lbl_currentKeyPressCheck.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_currentKeyPressCheck.Name = "lbl_currentKeyPressCheck";
-            this.lbl_currentKeyPressCheck.Size = new System.Drawing.Size(0, 13);
+            this.lbl_currentKeyPressCheck.Size = new System.Drawing.Size(0, 25);
             this.lbl_currentKeyPressCheck.TabIndex = 2;
             // 
             // pb_apple
             // 
             this.pb_apple.BackColor = System.Drawing.Color.Red;
-            this.pb_apple.Location = new System.Drawing.Point(252, 147);
+            this.pb_apple.Location = new System.Drawing.Point(504, 283);
+            this.pb_apple.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pb_apple.Name = "pb_apple";
-            this.pb_apple.Size = new System.Drawing.Size(15, 15);
+            this.pb_apple.Size = new System.Drawing.Size(30, 29);
             this.pb_apple.TabIndex = 4;
             this.pb_apple.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1568, 1079);
             this.Controls.Add(this.pb_apple);
             this.Controls.Add(this.lbl_currentKeyPressCheck);
             this.Controls.Add(this.pb_playerHead);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
