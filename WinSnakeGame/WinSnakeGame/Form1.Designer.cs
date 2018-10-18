a193cca77db40fdea3cb01e8e1128f4dbe0ab14b
shady901 <alex.j.hill13@gmail.com>
2018-10-18 14:56:11 +1300

    first

---
+++ WinSnakeGame/WinSnakeGame/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WinSnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Update = new System.Windows.Forms.Timer(this.components);
+            this.pb_playerHead = new System.Windows.Forms.PictureBox();
+            this.lbl_currentKeyPressCheck = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_playerHead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Update
+            // 
+            this.Update.Enabled = true;
+            this.Update.Tick += new System.EventHandler(this.Update_Tick);
+            // 
+            // pb_playerHead
+            // 
+            this.pb_playerHead.BackColor = System.Drawing.Color.Green;
+            this.pb_playerHead.Location = new System.Drawing.Point(351, 278);
+            this.pb_playerHead.Name = "pb_playerHead";
+            this.pb_playerHead.Size = new System.Drawing.Size(25, 25);
+            this.pb_playerHead.TabIndex = 1;
+            this.pb_playerHead.TabStop = false;
+            // 
+            // lbl_currentKeyPressCheck
+            // 
+            this.lbl_currentKeyPressCheck.AutoSize = true;
+            this.lbl_currentKeyPressCheck.Location = new System.Drawing.Point(348, 9);
+            this.lbl_currentKeyPressCheck.Name = "lbl_currentKeyPressCheck";
+            this.lbl_currentKeyPressCheck.Size = new System.Drawing.Size(0, 13);
+            this.lbl_currentKeyPressCheck.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lbl_currentKeyPressCheck);
+            this.Controls.Add(this.pb_playerHead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_playerHead)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer Update;
+        private System.Windows.Forms.PictureBox pb_playerHead;
+        private System.Windows.Forms.Label lbl_currentKeyPressCheck;
+    }
+}
+
---
+++ WinSnakeGame/WinSnakeGame/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSnakeGame
+{
+    public partial class Form1 : Form
+    {
+        public event KeyPressEventHandler KeysPress;
+        static string currentKey;
+        static int MovementAngle; //90,180,270,360
+        static int x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        private void Btn_start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Update_Tick(object sender, EventArgs e)
+        {
+            lbl_currentKeyPressCheck.Text = currentKey ;
+            pb_playerHead.Left += x;
+            pb_playerHead.Top += y;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            currentKey =Convert.ToString( e.KeyChar).ToLower();
+            if (currentKey == "a")
+            {
+                x = -2;
+                y = 0;
+            }
+            if (currentKey == "w")
+            {
+                x = 0;
+                y = -2;
+            }
+            if (currentKey == "s")
+            {
+                x = 0;
+                y = +2;
+            }
+            if (currentKey == "d")
+            {
+                x = +2;
+                y = 0;
+            }
+        }
+    }
+}
